eead5861436775b8b73d3c58b9c89cd7c9c07cf2
yinzishao <370953598@qq.com>
2016-04-16 23:54:12 +0800

    4/16

--- minicms/News/migrations/0001_initial.py
+++ minicms/News/migrations/0001_initial.py
@@ -2,13 +2,11 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
@@ -17,16 +15,10 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(max_length=256, verbose_name='\u6807\u9898')),
-                ('slug', models.CharField(max_length=256, verbose_name='\u7f51\u5740', db_index=True)),
-                ('content', models.TextField(default='', verbose_name='\u5185\u5bb9', blank=True)),
-                ('published', models.BooleanField(default=True, verbose_name='\u6b63\u5f0f\u53d1\u5e03')),
-                ('author', models.ForeignKey(verbose_name='\u4f5c\u8005', blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+                ('content', models.TextField(verbose_name='\u5185\u5bb9')),
+                ('pub_date', models.DateTimeField(auto_now_add=True, verbose_name='\u53d1\u8868\u65f6\u95f4')),
+                ('update_time', models.DateTimeField(auto_now=True, verbose_name='\u66f4\u65b0\u65f6\u95f4', null=True)),
             ],
-            options={
-                'verbose_name': '\u6559\u7a0b',
-                'verbose_name_plural': '\u6559\u7a0b',
-            },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Column',
@@ -41,12 +33,20 @@ class Migration(migrations.Migration):
                 'verbose_name': '\u680f\u76ee',
                 'verbose_name_plural': '\u680f\u76ee',
             },
-            bases=(models.Model,),
         ),
-        migrations.AddField(
-            model_name='article',
-            name='column',
-            field=models.ManyToManyField(to='News.Column', verbose_name='\u5f52\u5c5e\u680f\u76ee'),
-            preserve_default=True,
+        migrations.CreateModel(
+            name='Publisher',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=30)),
+                ('address', models.CharField(max_length=50)),
+                ('city', models.CharField(max_length=60)),
+                ('state_province', models.CharField(max_length=30)),
+                ('country', models.CharField(max_length=50)),
+                ('website', models.URLField()),
+            ],
+            options={
+                'ordering': ['name'],
+            },
         ),
     ]
